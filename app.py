9922fa63fbd4e703487421491c7166f67c8f94e6
Sreyas <83900600+codebysreyas@users.noreply.github.com>
2026-03-20 14:25:42 +0530

    Change socketio.run to enable debug mode

--- app.py
+++ app.py
@@ -475,5 +475,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port, debug=False)
+    socketio.run(app, debug=True)
