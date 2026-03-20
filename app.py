309f2077986e7578448faf96f72e52b9ee83e0ba
Sreyas <83900600+codebysreyas@users.noreply.github.com>
2026-03-20 12:59:17 +0530

    Update socketio.run to use environment variables

--- app.py
+++ app.py
@@ -474,4 +474,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)), debug=False)
