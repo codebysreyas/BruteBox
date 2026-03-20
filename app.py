77028f2e58ba0afa6fe790e3a9f86a3516083528
Sreyas <83900600+codebysreyas@users.noreply.github.com>
2026-03-20 14:11:01 +0530

    Change socketio port to 5000 and clean up code

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key-change-in-production'
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 active_tasks = {}
 pause_events = {}
@@ -475,4 +475,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)), debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=False)
